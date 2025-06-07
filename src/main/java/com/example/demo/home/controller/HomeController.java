0271de5e9c79ebdf4e58dc7dc37ad4881acce5d0
caroa <caroan@naver.com>
2025-06-07 19:09:28 +0900

    test

--- src/main/java/com/example/demo/home/controller/HomeController.java
+++ src/main/java/com/example/demo/home/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	@GetMapping("/")
 	public String getHome() {
 		String home = "/home/view/home";
-		System.out.println("카카오톡에 'caroan github 비빌번호' 이름으로 비밀번호 있으니 찾아볼 것"); 
+		System.out.println("카카오톡에 'caroan github 비빌번호' 이름으로 비밀번호 있으니 찾아볼 것");
 		return home;
 	}
 }
